e018f6b089916c5afd9702cc229bce2e295fb699
Constantin Clerc <constantin@cclerc.ch>
2024-04-17 18:25:19 +0200

    sshBridge initial commit

---
+++ sshBridge.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect -f
+# Constantin CLERC - v0.2
+
+# Parse command-line arguments
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+set server [lindex $argv 2]
+
+# Enable host key checking to automatically trust the host
+spawn ssh -o StrictHostKeyChecking=no -D 8080 -C -N $username@$server
+
+expect "assword:"
+send "$password\r"
+
+interact

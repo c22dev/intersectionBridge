ee2a81e7826b55ed032efc32996ae798b7982058
c22dev <constclerc@gmail.com>
2024-04-17 21:29:42 +0200

    added version checking, launchagents and installer

---
+++ installer.sh
@@ -0,0 +1,14 @@
+cd $HOME
+mkdir Intersection
+cd Intersection
+curl https://raw.githubusercontent.com/c22dev/intersectionBridge/main/version > .version
+curl https://raw.githubusercontent.com/c22dev/intersectionBridge/main/intersectiond.sh > intersectiond.sh
+chmod a+x intersectiond.sh
+
+cd "$HOME/Library/LaunchAgents/"
+curl https://raw.githubusercontent.com/c22dev/intersectionBridge/main/ch.cclerc.intersection.plist > ch.cclerc.intersection.plist
+# Taken from one of my previous code, idk how I wrote that
+awk -v home="$HOME" '{gsub("/Users/REPLACEUSERHERE", home)} 1' "$HOME/Library/LaunchAgents/ch.cclerc.intersection.plist" > tmpfile && mv tmpfile "$HOME/Library/LaunchAgents/ch.cclerc.intersection.plist"
+launchctl load ~/Library/LaunchAgents/ch.cclerc.intersection.plist
+
+touch "$HOME/.intersectionHasBeenRan"

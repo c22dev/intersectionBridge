0307c57e8b31841fb2ff32503ed15a6362ab1b84
c22dev <102235607+c22dev@users.noreply.github.com>
2024-06-06 11:16:33 +0200

    Update da updater

--- updater.sh
+++ updater.sh
@@ -8,13 +8,13 @@ if curl -I --socks5-hostname localhost:8080 github.com --max-time 10 >/dev/null 
     echo "Ready to update"
     sleep 2
     cd $HOME/Intersection
-    if [ "$(cat .version)" != "$(curl -s "https://raw.githubusercontent.com/c22dev/intersectionBridge/main/version")" ]; then
+    if [ "$(cat .version)" != "$(curl --socks5-hostname localhost:8080 -s "https://raw.githubusercontent.com/c22dev/intersectionBridge/main/version")" ]; then
         echo "You are running an outdated version of the main script."
         echo "Updating..."
         choiceUpd=$(osascript -e 'button returned of (display dialog "An update is available. Do you want to update ?" buttons {"Yes", "No"} default button "Yes")')
         if [ "$choiceUpd" = "Yes" ]; then
-            curl https://raw.githubusercontent.com/c22dev/intersectionBridge/main/intersectiond.sh > intersectiond.sh
-            curl https://raw.githubusercontent.com/c22dev/intersectionBridge/main/version > .version
+            curl --socks5-hostname localhost:8080 https://raw.githubusercontent.com/c22dev/intersectionBridge/main/intersectiond.sh > intersectiond.sh
+            curl --socks5-hostname localhost:8080 https://raw.githubusercontent.com/c22dev/intersectionBridge/main/version > .version
             chmod a+x intersectiond.sh
             doTheSizeChecks() {
                 sizeOfMain=$(wc -c intersectiond.sh | awk '{print $1}')
@@ -22,14 +22,14 @@ if curl -I --socks5-hostname localhost:8080 github.com --max-time 10 >/dev/null 
                 if [ "$sizeOfMain" -ge "1000" ]; then
                     echo "Size seems correct"
                 else
-                    curl https://raw.githubusercontent.com/c22dev/intersectionBridge/main/intersectiond.sh > intersectiond.sh
+                    curl --socks5-hostname localhost:8080 https://raw.githubusercontent.com/c22dev/intersectionBridge/main/intersectiond.sh > intersectiond.sh
                     chmod a+x intersectiond.sh
                     doTheSizeChecks
                 fi
                 if [ "$sizeOfVersion" -ge "1" ]; then
                     echo "Size seems correct"
                 else
-                    curl https://raw.githubusercontent.com/c22dev/intersectionBridge/main/version > .version
+                    curl --socks5-hostname localhost:8080 https://raw.githubusercontent.com/c22dev/intersectionBridge/main/version > .version
                     doTheSizeChecks
                 fi
             }
